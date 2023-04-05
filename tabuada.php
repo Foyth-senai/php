854b695f377b78f6d0f90dfc0f452b9b55409930
Foyth-senai <124690153+Foyth-senai@users.noreply.github.com>
2023-04-05 12:03:23 -0300

    a

---
+++ tabuada.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tabuada</title>
+    <style>
+        .verde{
+            background-color: green;
+        }
+        .amarelo{
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+    <br>
+    <?php
+      //  echo("Para abrir cole no navegador LOCALHOST:8080/PHP-1/tabuada.php");
+        $valor = $_POST["valor"];
+
+      for($x=1;$x<=10;$x++)
+      {
+        $resultado = ($valor * $x);
+        if($resultado%2 == 0)
+        {
+            echo("<div class='verde'>");
+        }
+        else{
+            echo("<div class='amarelo'>");
+        }
+        echo("$valor x $x = $resultado <br>");
+        echo("</div>");
+      }
+    ?>
+</body>
+</html>
